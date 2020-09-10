52f3e29e0d8de325074f6463263b597b8df02a5b
ouyangli <oyl@webull.com>
2020-09-10 20:10:40 +0800

    add some comment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './index.css'
 import {join} from 'lodash'
 function component() {
 	const element = document.createElement("h1")
-
+	// Here is just to lodash  chunk
 	// Lodash, currently included via a script, is required for this line to work
 	element.innerHTML = join(["Hello", "webpack"], " ")
 
